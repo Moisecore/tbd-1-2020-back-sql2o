80bc46f1d392df9eb9bedc1271bab6e99f54cbc5
Moisecore <moises.garcia@usach.cl>
2020-06-06 00:57:24 -0400

    Commit inicial

---
+++ src/test/java/tbd/grupo_seis/voluntariado/VoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package tbd.grupo_seis.voluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
